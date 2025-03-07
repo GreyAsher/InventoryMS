957fe6aab807b7b47c3e940778bfcaae63b09c11
GreyAsher <mopadaadrian@gmail.com>
2025-03-07 12:20:32 +0800

    Up

---
+++ InventoryMS/Data/DBContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryMS.Model;
+
+namespace InventoryMS.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base("name=InventoryDB") { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<StockManagement> StockManagement { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<SalesReport> SalesReports { get; set; }
+        public DbSet<PurchaseReport> PurchaseReports { get; set; }
+        public DbSet<StockReport> StockReports { get; set; }
+        public DbSet<DashboardSummary> DashboardSummaries { get; set; }
+        public DbSet<DashboardGraphData> DashboardGraphData { get; set; }
+        public DbSet<DailySalesSummary> DailySalesSummaries { get; set; }
+        public DbSet<MonthlyTrends> MonthlyTrends { get; set; }
+        public DbSet<BestSellingProduct> BestSellingProducts { get; set; }
+        public DbSet<LowStockAlert> LowStockAlerts { get; set; }
+
+        
+    }
+}
--- InventoryMS/Model/Product.cs
+++ InventoryMS/Model/Product.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InventoryMS.Model;
 
 namespace InventoryMS.Model
 {
-    internal class Product
+    public class Product
     {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string ProductName { get; set; }
+
+        [ForeignKey("Category")]
+        public int? CategoryID { get; set; }
+
+        [ForeignKey("Supplier")]
+        public int? SupplierID { get; set; }
+
+        public int QuantityInStock { get; set; } = 0;
+        public int ReorderLevel { get; set; } = 10;
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal UnitPrice { get; set; }
+
+        [MaxLength(50)]
+        public string UnitOfMeasure { get; set; }
+
+        [MaxLength(255)]
+        public string Barcode { get; set; }
+
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        // Navigation properties
+        public virtual Category Category { get; set; }
+        public virtual Supplier Supplier { get; set; }
     }
 }
