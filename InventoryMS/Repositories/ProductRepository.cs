9a6fb1cdf8e500a563abf1da8485a4f916a1c57b
GreyAsher <mopadaadrian@gmail.com>
2025-03-07 13:17:58 +0800

    Up

---
+++ InventoryMS/Repositories/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryMS.Model;
+using InventoryMS.Data;
+
+namespace InventoryMS.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly DBContext _context;
+
+        public ProductRepository(DBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            return await _context.Products.Include(p => p.Category).Include(p => p.Supplier).ToListAsync();
+        }
+
+        public async Task<Product> GetProductById(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProduct(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
